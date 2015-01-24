24728329091280bde49fe21c49fabed22f77400c
orenstal <orenstal@gmail.com>
2015-01-24 13:46:54 +0200

    after merging our versions, we have the following issues: 1. although we pass our 'test.js' file, we receive two error in the end. 2. the things we talked about earlier (body params, next, sending 404 in case that no one do so, etc).

--- hujinet.js
+++ hujinet.js
@@ -54,9 +54,9 @@ exports.createServer = function(port, onRequestArrival, callBackFunc) {
 
 
         socket.setTimeout(2000, function() {
-            //if (response.isSent == false) {
-            //    response.status(404).send();
-            //}
+            if (response.isSent == false) {
+                response.status(404).send();
+            }
 
             socket.end();
         });
